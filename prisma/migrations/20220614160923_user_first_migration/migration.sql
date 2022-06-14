db85b416ac75598d14bd05fbda4dbc731f16dc5c
Ryan Izac <io.ryan@outlook.com>
2022-06-14 14:39:46 -0300

    prisma database configured

---
+++ prisma/migrations/20220614160923_user_first_migration/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "cellphone" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "validated" BOOLEAN NOT NULL DEFAULT false,
+    "active" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_id_key" ON "User"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
