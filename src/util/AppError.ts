a71688535414236aa1f88d2acf1da747f3e1735b
Ryan Izac <io.ryan@outlook.com>
2022-06-14 14:40:08 -0300

    AppError developed

---
+++ src/util/AppError.ts
@@ -0,0 +1,17 @@
+interface Struct<T> {
+  code: number;
+  message: string;
+  info?: T;
+}
+
+export default class AppError<T extends object> implements Struct<T> {
+  code: number;
+  message: string;
+  info?: T;
+
+  constructor(props: Struct<T>) {
+    this.code = props.code;
+    this.message = props.message;
+    if (props?.info) this.info = props.info;
+  }
+}
