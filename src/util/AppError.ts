fb632219e92a25707c753a9aa9f6661a04134efd
Ryan Izac <io.ryan@outlook.com>
2022-06-20 12:40:04 -0300

    up: AppError with default code

--- src/util/AppError.ts
+++ src/util/AppError.ts
@@ -1,16 +1,16 @@
 interface Struct<T> {
-  code: number;
+  code?: number;
   message: string;
   info?: T;
 }
 
 export default class AppError<T extends object> implements Struct<T> {
-  code: number;
+  code?: number;
   message: string;
   info?: T;
 
   constructor(props: Struct<T>) {
-    this.code = props.code;
+    this.code = props.code || 400;
     this.message = props.message;
     if (props?.info) this.info = props.info;
   }
